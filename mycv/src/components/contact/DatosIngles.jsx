d7a36d0d2b19e4eaf1eac63691c846ad002a290b
Iosu Gómez <131406997+IosuVitoria@users.noreply.github.com>
2023-07-05 17:03:29 +0200

    contacto hecho

---
+++ mycv/src/components/contact/DatosIngles.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { CV } from '../../MyCV/CV';
+
+
+const DatosIngles = () => {
+  const { DatosGeneralesEN, Contacto } = CV;
+  const { name, last_name, career, interests } = DatosGeneralesEN;
+
+  return (
+    <div>
+      <h2>General Information:</h2>
+      <strong><p>Name: {name} {last_name}</p></strong>
+      <strong><p>{career}</p></strong>
+      <p>Interests: {interests}</p>
+      <h2>Contact Information:</h2>
+      <p>Address: {Contacto.dirección}</p>
+      <strong><p>
+        Email: <a href={`mailto:${Contacto.gmail}`}>{Contacto.gmail}</a>
+      </p></strong>
+      <strong><p>LinkedIn: {Contacto.linkedin}</p></strong>
+      <p>Own Car: {Contacto["coche propio"]}</p>
+    </div>
+  );
+};
+
+export default DatosIngles;
